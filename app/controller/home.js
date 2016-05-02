102d94b8b8551b6c3acdce2a4936fc9b5e616555
chinaTinker <xuyifeng11@gmail.com>
2016-05-02 14:51:51 +0800

    add the goods

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const cases = require('../../lib/success_case');
+const goods = require('../../lib/hot_goods');
 
 exports.index = function* (){
 	
   const data = {
     active: 'active',
-    cases: cases()
+    cases: cases(),
+    goods: goods(),
   };
 
 	yield this.render('index', data);	
